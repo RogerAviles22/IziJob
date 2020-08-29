58c1002f937201656b1d3144e713c7dbb1e9f7fb
Roger Avilés G <rogiavil@espol.edu.ec>
2020-08-29 11:20:17 -0500

    Filtro por Categoria implementado

--- lib/empleoVista.dart
+++ lib/empleoVista.dart
@@ -12,6 +12,7 @@ class EmpleoVista extends StatefulWidget {
 
 class _EmpleoVistaState extends State<EmpleoVista> {
   List<Empleo> empleoList = [];
+  List<Empleo> filteredEmpleoList = [];
   bool isBusqueda = false;
 
   @override
@@ -38,12 +39,26 @@ class _EmpleoVistaState extends State<EmpleoVista> {
             data[individualKey]['sueldo'],
             data[individualKey]['vacantes']);
 
-        empleoList.add(empleo);
-      }
+        /*empleoList.add(empleo);
+        filteredEmpleoList.add(empleo);*/
+        //Agrega y ordena
+        setState(() {
+          empleoList.add(empleo);
+          filteredEmpleoList.add(empleo);
+          empleoList.sort((a, b) => b.fechaPublicado.compareTo(a.fechaPublicado));
+          filteredEmpleoList.sort((a, b) => b.fechaPublicado.compareTo(a.fechaPublicado));
+        });
 
-      setState(() {
-        print("Longitud: $empleoList.length");
-      });
+      }
+    });
+  }
+
+  void _filterEmpleos(value) {
+    setState(() {
+      filteredEmpleoList = empleoList
+          .where((empleos) =>
+              empleos.categoria.toLowerCase().contains(value.toLowerCase()))
+          .toList();
     });
   }
 
@@ -57,7 +72,7 @@ class _EmpleoVistaState extends State<EmpleoVista> {
             ? Text('Empleo')
             : TextField(
                 onChanged: (value) {
-                  print(value);
+                  _filterEmpleos(value);
                 },
                 style: TextStyle(color: Colors.white),
                 decoration: InputDecoration(
@@ -75,6 +90,7 @@ class _EmpleoVistaState extends State<EmpleoVista> {
                   onPressed: () {
                     setState(() {
                       this.isBusqueda = false;
+                      filteredEmpleoList = empleoList;
                     });
                   },
                 )
@@ -91,12 +107,27 @@ class _EmpleoVistaState extends State<EmpleoVista> {
 
       body: Container(
           //padding: const EdgeInsets.all(5.0),
-          child: empleoList.length == 0
-              ? Text("No hay Empleos :(")
+          child: filteredEmpleoList.length == 0
+              ? Center(
+                  child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text("No hay Empleos :(",
+                            style: TextStyle(
+                              fontFamily: 'Varela',
+                              fontSize: 18.0,
+                              fontWeight: FontWeight.bold,
+                            )),
+                        SizedBox(
+                          height: 20.0,
+                        ),
+                        CircularProgressIndicator()
+                      ]),
+                )
               : ListView.builder(
-                  itemCount: empleoList.length,
+                  itemCount: filteredEmpleoList.length,
                   itemBuilder: (_, index) {
-                    return postsEmpleo(empleoList[index]
+                    return postsEmpleo(filteredEmpleoList[index]
                         /*empleoList[index].titulo,
                         empleoList[index].fechaPublicado,
                         empleoList[index].descripcion,
@@ -121,76 +152,75 @@ class _EmpleoVistaState extends State<EmpleoVista> {
 
   Widget postsEmpleo(Empleo empleo) {
     return Card(
-      elevation: 10.0,
-      margin: EdgeInsets.all(14.0),  
-      child: InkWell(
-        onTap: () {
-        Navigator.push(context,
-          MaterialPageRoute(
-            builder: (context) => DetailEmpleo(empleo: empleo),
-          ),
-        );
-      },
-        child:  Container(
-        padding: EdgeInsets.all(14.0),        
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        elevation: 10.0,
+        margin: EdgeInsets.all(14.0),
+        child: InkWell(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => DetailEmpleo(empleo: empleo),
+              ),
+            );
+          },
+          child: Container(
+            padding: EdgeInsets.all(14.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
-                Text(
-                  empleo.titulo,
-                  style: TextStyle(
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text(
+                      empleo.titulo,
+                      style: TextStyle(
                         fontFamily: 'Varela',
-                        fontSize: 21.0,
+                        fontSize: 15.0,
                         fontWeight: FontWeight.bold,
                       ),
-                  //style: Theme.of(context).textTheme.subtitle1,
-                  textAlign: TextAlign.center,
+                      //style: Theme.of(context).textTheme.subtitle1,
+                      textAlign: TextAlign.center,
+                    ),
+                    Text(
+                      empleo.fechaPublicado,
+                      style: Theme.of(context).textTheme.subtitle2,
+                      textAlign: TextAlign.center,
+                    )
+                  ],
+                ),
+                SizedBox(
+                  height: 15.0,
                 ),
                 Text(
-                  empleo.fechaPublicado,
-                  style: Theme.of(context).textTheme.subtitle2,
+                  empleo.descripcion,
+                  style: Theme.of(context).textTheme.bodyText2,
                   textAlign: TextAlign.center,
-                )
-              ],
-            ),
-            SizedBox(
-              height: 15.0,
-            ),
-            Text(
-              empleo.descripcion,
-              style: Theme.of(context).textTheme.bodyText2,
-              textAlign: TextAlign.center,
-            ),
-            SizedBox(
-              height: 10.0,
-            ),
-            Text(
-              "Categoría: " + empleo.categoria,
-              style: Theme.of(context).textTheme.caption,
-              textAlign: TextAlign.left,
-            ),
-            SizedBox(
-              height: 10.0,
-            ),
-            Align(
-              alignment: Alignment.centerRight,
-              child: Text(
-                "Vacantes: " + empleo.vacantes,
-                style: Theme.of(context).textTheme.caption,
-                /*style: TextStyle(
+                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                Text(
+                  "Categoría: " + empleo.categoria,
+                  style: Theme.of(context).textTheme.caption,
+                  textAlign: TextAlign.left,
+                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: Text(
+                    "Vacantes: " + empleo.vacantes,
+                    style: Theme.of(context).textTheme.caption,
+                    /*style: TextStyle(
                         fontFamily: 'Varela',
                         fontSize: 21.0,
                       )*/
-              ),
-            )
-          ],
-        ),
-      ), 
-    
-      )    
-      );
+                  ),
+                )
+              ],
+            ),
+          ),
+        ));
   }
 }
