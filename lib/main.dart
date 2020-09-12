8bea5847a22ae9c1157757d156291d180993996c
Roger Avilés G <rogiavil@espol.edu.ec>
2020-09-12 14:47:24 -0500

    Agregado gif, libreria y clase para SplashScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:izijob/misc/MainScreen.dart';
 //import 'package:izijob/footer.dart';
 //mport 'package:izijob/registroVista.dart';
 
-import 'login.dart';
 
 void main() {
   runApp(MyApp());
@@ -15,14 +15,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Izi Job',
       theme: ThemeData(
-        //primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      //home: Footer(),
-      home: Login(),
+      //home: Footer(),s
+      home: MainScreen(),
       debugShowCheckedModeBanner: false,
     );
   }
